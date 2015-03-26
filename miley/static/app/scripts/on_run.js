866b3c5db068df61d7e89f18cd41569bf31afb6b
valish <andrew@vali.sh>
2015-03-25 17:29:48 -0700

    adding on_run.js for logic to be executed on app.run

---
+++ miley/static/app/scripts/on_run.js
@@ -0,0 +1,22 @@
+'use strict';
+
+/**
+ * @ngInject
+ */
+function OnRun($rootScope, AppSettings) {
+
+  // change page title based on state
+  $rootScope.$on('$stateChangeSuccess', function(event, toState) {
+    $rootScope.pageTitle = '';
+
+    if ( toState.title ) {
+      $rootScope.pageTitle += toState.title;
+      $rootScope.pageTitle += ' \u2015 ';
+    }
+
+    $rootScope.pageTitle += AppSettings.appTitle;
+  });
+
+}
+
+module.exports = OnRun;
