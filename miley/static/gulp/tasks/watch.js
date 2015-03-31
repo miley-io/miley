696a99acddfa29206ea917cf71a38750be44c556
valish <andrew@vali.sh>
2015-03-30 23:25:32 -0700

    removing browserSync, views from gulp watch task

--- miley/static/gulp/tasks/watch.js
+++ miley/static/gulp/tasks/watch.js
@@ -3,11 +3,10 @@
 var config        = require('../config');
 var gulp          = require('gulp');
 
-    gulp.task('watch', ['browserSync', 'server'], function() {
+    gulp.task('watch', ['server'], function() {
 
     gulp.watch(config.scripts.src, ['lint', 'browserify']);
     gulp.watch(config.styles.src,  ['styles']);
     gulp.watch(config.images.src,  ['images', 'reload']);
-    gulp.watch(config.views.watch, ['views']);
 
 });
