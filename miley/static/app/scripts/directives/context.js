f1b8991ee0a501d61863953700bb49bc6dcb7493
valish <andrew@vali.sh>
2015-03-31 09:16:35 -0700

    showing missions in workspace, using workspace service in context directive

--- miley/static/app/scripts/directives/context.js
+++ miley/static/app/scripts/directives/context.js
@@ -6,15 +6,18 @@ function context() {
   return {
     restrict: 'A',
 
-    controller: function($scope, Session) {
+    controller: function($scope, Session, Workspace) {
       // On (all) pageload, fetch session
       Session.get().then(function (session) {
         Session.session = session;
         $scope.session = session;
 
-        /* body class */
         if ($scope.session.logged) {
           $scope.layout = 'l-private';
+          // Get missions
+          Workspace.missions().then(function (missions) {
+            $scope.missions = missions;
+          })
         } else {
           $scope.layout = 'l-public';
         }
