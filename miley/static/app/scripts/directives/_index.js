d3240e5533e3790bee43e6db7d0a010930a3825b
valish <andrew@vali.sh>
2015-04-07 23:36:35 -0700

    requiring mission-roadmap directive

--- miley/static/app/scripts/directives/_index.js
+++ miley/static/app/scripts/directives/_index.js
@@ -8,3 +8,4 @@ module.exports = angular.module('miley.directives', []);
 // Define the list of directives here
 require('./context.js');
 require('./workspace.js');
+require('./mission_roadmap.js');
