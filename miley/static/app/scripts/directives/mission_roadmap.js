ce0e758611b9a4a8dc402e79b493b23d5c0be4f2
valish <andrew@vali.sh>
2015-04-07 23:33:50 -0700

    adding mission-roadmap directive

---
+++ miley/static/app/scripts/directives/mission_roadmap.js
@@ -0,0 +1,19 @@
+'use strict';
+
+var directives = require('./_index.js');
+
+function missionRoadmap() {
+  return {
+    restrict: 'A',
+    controller: function($scope, $element, $attrs, $timeout, Mission) {
+      $timeout(function() {
+        var mid = $attrs.mission;
+        Mission.milestones(mid).then(function(data) {
+          $scope.milestones = data;
+        })
+      }, 0);
+    }
+  }
+}
+
+directives.directive('missionRoadmap', missionRoadmap);
